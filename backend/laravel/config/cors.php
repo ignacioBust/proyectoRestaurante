5420f5c96ba477b832eb9d004b9adb48fdc68085
Ignacio <nachobustos@live.com.ar>
2026-03-29 03:55:40 -0300

    fix: actualizar CORS para produccion

--- backend/laravel/config/cors.php
+++ backend/laravel/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://proyecto-restaurante-sigma.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
